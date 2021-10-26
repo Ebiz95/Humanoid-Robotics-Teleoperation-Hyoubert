1208e9c83d50bfe888992b953714ff484a474e66
Sara980710 <sara@idala.eu>
2021-10-26 21:17:56 +0200

    correct variables

--- Inverse_kinematics_python/model.py
+++ Inverse_kinematics_python/model.py
@@ -52,7 +52,7 @@ def plot_base(ax, L):
             [from_pos[2], to_pos[2]], c="k", alpha=alpha)
 
 
-def plot_result(ax, L, P2, P3, P4, P5):
+def plot_result(ax, L, P1, P2, P3, P4, P5):
     # Torso
     from_pos = [0, 0, -L[1]]
     to_pos = [0, 0, 0]
@@ -60,7 +60,7 @@ def plot_result(ax, L, P2, P3, P4, P5):
             [from_pos[2], to_pos[2]], c="b")
 
     # Sholders
-    from_pos = [-P2[0], -P2[1], P2[2]]
+    from_pos = P1
     to_pos = P2
     ax.plot([from_pos[0], to_pos[0]], [from_pos[1], to_pos[1]],
             [from_pos[2], to_pos[2]], c="orange")
@@ -86,43 +86,48 @@ def plot_result(ax, L, P2, P3, P4, P5):
 
 def forward_kinematics(theta, link):
     # forw kinematics
-    trans_fromP1 = np.array([[math.cos(theta[0]), -math.sin(theta[0]), 0, 0],
+    frame_a_to_0 = np.array([[math.cos(theta[0]), -math.sin(theta[0]), 0, 0],
                             [math.sin(theta[0]), math.cos(theta[0]), 0, 0],
                             [0, 0, 1, 0],
                             [0, 0, 0, 1]])
-    trans_fromP2 = np.array([[1, 0, 0, 0],
+    frame_b_to_a = np.array([[1, 0, 0, 0],
                             [0, 1, 0, -L[2]],
                             [0, 0, 1, 0],
                             [0, 0, 0, 1]])
-    trans_fromP3 = np.array([[1, 0, 0, L[3]],
+    frame_1_to_b = np.array([[1, 0, 0, L[3]],
                             [0, 1, 0, 0],
                             [0, 0, 1, 0],
                             [0, 0, 0, 1]])
-    trans_fromP4 = np.array([[math.cos(theta[1]), -math.sin(theta[1]), 0, link[0]*math.cos(theta[1])],
+    frame_c_to_1 = np.array([[math.cos(theta[1]), -math.sin(theta[1]), 0, L[4]*math.cos(theta[1])],
                             [0, 0, -1, 0],
-                            [math.sin(theta[1]), math.cos(theta[1]),
-                            0, link[0]*math.sin(theta[1])],
+                            [math.sin(theta[1]), math.cos(theta[1]), 0, L[4]*math.sin(theta[1])],
                             [0, 0, 0, 1]])
-    trans_fromP5 = np.array([[math.cos(theta[2]), -math.sin(theta[2]), 0, link[1]*math.cos(theta[2])],
-                            [math.sin(theta[2]), math.cos(theta[2]),
-                            0, link[1]*math.sin(theta[2])],
+    frame_d_to_c = np.array([[math.cos(theta[2]), -math.sin(theta[2]), 0, U3*math.cos(theta[2])],
+                            [math.sin(theta[2]), math.cos(theta[2]), 0, U3*math.sin(theta[2])],
                             [0, 0, 1, 0],
                             [0, 0, 0, 1]])
 
     target_frame = [0, 0, 0, 1]
 
-    P1 = np.matmul(trans_fromP1, target_frame)
+    P2 = np.matmul(frame_a_to_0, 
+         np.matmul(frame_b_to_a, target_frame))
+    P1 = [-P2[0], -P2[1], P2[2]]
     print(f"{P1 = }")
-    P2 = np.matmul(trans_fromP1, np.matmul(trans_fromP2, target_frame))
     print(f"{P2 = }")
-    P3 = np.matmul(trans_fromP1, np.matmul(
-        trans_fromP2, np.matmul(trans_fromP3, target_frame)))
+    P3 = np.matmul(frame_a_to_0, 
+         np.matmul(frame_b_to_a, 
+         np.matmul(frame_1_to_b, target_frame)))
     print(f"{P3 = }")
-    P4 = np.matmul(trans_fromP1, np.matmul(trans_fromP2, np.matmul(
-        trans_fromP3, np.matmul(trans_fromP4, target_frame))))
+    P4 = np.matmul(frame_a_to_0, 
+         np.matmul(frame_b_to_a, 
+         np.matmul(frame_1_to_b, 
+         np.matmul(frame_c_to_1, target_frame))))
     print(f"{P4 = }")
-    P5 = np.matmul(trans_fromP1, np.matmul(trans_fromP2, np.matmul(trans_fromP3, np.matmul(
-        trans_fromP4, np.matmul(trans_fromP5, target_frame)))))
+    P5 = np.matmul(frame_a_to_0, 
+         np.matmul(frame_b_to_a, 
+         np.matmul(frame_1_to_b, 
+         np.matmul(frame_c_to_1, 
+         np.matmul(frame_d_to_c, target_frame)))))
     print(f"{P5 = }")
 
     return P1, P2, P3, P4, P5
@@ -134,74 +139,80 @@ def restrict(target, max, min):
         target = min
     return target
 
-def calculate_angles(pos, link, previous_theta, ax):
+def calculate_angles(pos, U3, previous_theta, ax):
     theta = np.zeros(3)
     out_of_reach = False
 
     # Calculate first theta
-    U1 = pos[0]
-    U2 = pos[1]
-
     if pos[0] < 0:
-        theta_a = -np.arctan(U1/U2)
-        U3 = np.sqrt(U1**2+U2**2)
-        term = U3**2-L[2]**2
-        if term < 0:
-            term = 0
-        U4 = np.sqrt(np.abs(U3**2-L[2]**2))
-        theta_b = np.arccos(L[2]/U3)
-
-        if pos[1] > 0:
-            theta[0] = np.pi + theta_a-theta_b
+        if pos[1] < 0:
+            theta_a = np.abs(np.arctan(pos[0]/pos[1]))
+            U1 = np.sqrt(pos[0]**2+pos[1]**2)
+            theta_b = np.arccos(L[2]/U1)
+            theta[0] = -theta_a - theta_b
         else:
-            theta[0] = theta_a-theta_b
+            theta_a = np.abs(np.arctan(pos[0]/pos[1]))
+            U1 = np.sqrt(pos[0]**2+pos[1]**2)
+            theta_b = np.arccos(L[2]/U1)
+            theta[0] = np.pi + theta_a - theta_b
     else:
-        theta_a = np.arctan(U2/U1)
-        U3 = np.sqrt(U1**2+U2**2)
-        U4 = np.sqrt(np.abs(U3**2-L[2]**2))
-        theta_b = np.arccos(L[2]/U3)
-        theta[0] = np.pi/2-(-theta_a+theta_b)
-    
+        if pos[1] < 0:
+            theta_a = np.abs(np.arctan(pos[1]/pos[0]))
+            U1 = np.sqrt(pos[0]**2+pos[1]**2)
+            theta_b = np.arccos(L[2]/U1)
+            theta[0] = np.pi/2 - (theta_a + theta_b)
+        else:
+            theta_a = np.abs(np.arctan(pos[1]/pos[0]))
+            U1 = np.sqrt(pos[0]**2+pos[1]**2)
+            theta_b = np.arccos(L[2]/U1)
+            theta[0] = np.pi/2 + theta_a - theta_b
+
+    # Arm Length
+    U2 = np.sqrt(U1**2-L[2]**2)
+
+
+    # Calculate third theta
+    pos_frame1 = [U2-L[3], 0, pos[2]]
+    print(f"{pos_frame1 = }")
+
+    U4 = np.sqrt(pos_frame1[0]**2 + pos_frame1[2]**2)
+    numerator = U4**2 - L[4]**2 - U3**2 
+    denominator = 2*L[4]*U3
+    theta[2] = np.arccos(numerator/denominator)
+
+    # Calculate second theta
+    theta_c = np.arctan(pos_frame1[2]/pos_frame1[0])
+    U5 = U3*np.sin(theta[2])
+    U6 = U3*np.cos(theta[2])
+    theta_d = np.arctan(U5/(L[4]+U6))
+    theta[1] = theta_c-theta_d
+
+    # Check if thetas are out of range
     restricted = restrict(theta[0], max_angles[0], min_angles[0])
     if not (restricted == theta[0]):
         out_of_reach = True
-        theta[0] = restricted
-
-    # Calculate third theta
-    print(f"{link = }")
-    target_frame3 = [U4-L[3], 0, pos[2]]
-    print(f"{target_frame3 = }")
-
-    nominator = target_frame3[0]**2 + \
-        target_frame3[2]**2 - link[0]**2 - link[1]**2
-    denominator = 2*link[0]*link[1]
-    theta[2] = np.arccos(nominator/denominator)
-
-    # Calculate second theta
-    restricted = restrict(theta[2], max_angles[2], min_angles[2])
-    if not (restricted == theta[2]):
-        out_of_reach = True
-        theta[2] = restricted
-
-    term = target_frame3[2]/target_frame3[0]
-    nominator = link[1]*np.sin(theta[2])
-    denominator = link[0]+link[1]*np.cos(theta[2])
-    theta[1] = np.arctan(term)-np.arctan(nominator/denominator)
 
     restricted = restrict(theta[1], max_angles[1], min_angles[1])
     if not (restricted == theta[1]):
         out_of_reach = True
-        theta[1] = restricted
 
+    restricted = restrict(theta[2], max_angles[2], min_angles[2])
+    if not (restricted == theta[2]):
+        out_of_reach = True
+
+    # Check if thetas has nan values
     for i,t in enumerate(theta):
         if np.isnan(t):
             out_of_reach = True
-            theta[i] = previous_theta[i]
 
     print(f"{theta = }")
 
+    # Notify that the position is invalid
     if out_of_reach:
         ax.text(0.1, 0.1, 0.5, "Out of reach!")
+        theta[0] = previous_theta[0]
+        theta[1] = previous_theta[1]
+        theta[2] = previous_theta[2]
     else:
         ax.text(0.1, 0.1, 0.5, "")
 
@@ -214,10 +225,7 @@ min_angles = np.array([-np.pi/2-np.pi/4, -np.pi/2-np.pi/4, -np.pi/2-np.pi/4])
 max_angles = np.array([np.pi/2+np.pi/4, np.pi/2+np.pi/4, np.pi/2+np.pi/4])
 
 pos = np.array([0.25, -0.11, -0.1])
-#pos = np.array([0.18, 0.15, 0.6])
-#pos = np.array([0.18, 0.15, 0.8])
-
-link = [L[4], np.sqrt((L[5] + L[6])**2 + L[7]**2)]
+U3 = np.sqrt((L[5] + L[6])**2 + L[7]**2)
 
 ### Create plot
 
@@ -236,10 +244,10 @@ ax.text(0.1, 0.1, 0.5, s="let's begin!")
 
 ### Calculate thetas
 theta = np.zeros(3)
-theta = calculate_angles(pos, link, theta, ax)
+theta = calculate_angles(pos, U3, theta, ax)
 
 ### Calculate resulting positions
-P1, P2, P3, P4, P5 = forward_kinematics(theta, link)
+P1, P2, P3, P4, P5 = forward_kinematics(theta, U3)
 
 ### Plot target
 ax.scatter3D(pos[0], pos[1], pos[2])
@@ -248,12 +256,14 @@ ax.scatter3D(pos[0], pos[1], pos[2])
 plot_base(ax,L)
 
 ### Plot result
-plot_result(ax,L, P2, P3, P4, P5)
+plot_result(ax, L, P1, P2, P3, P4, P5)
 
 def update_wave(val):
     print(" ")
     theta = np.zeros(3)
     pos = np.array([sliderx.val, slidery.val, sliderz.val])
+    print(f"{pos = }")
+
     ax.cla()
     ax.set_xlim3d(ax_min[0], ax_max[0])
     ax.set_ylim3d(ax_min[1], ax_max[1])
@@ -263,10 +273,10 @@ def update_wave(val):
     ax.set_zlabel("Z")
 
     ### Calculate thetas
-    theta = calculate_angles(pos, link, theta, ax)
+    theta = calculate_angles(pos, U3, theta, ax)
 
     ### Calculate resulting positions
-    P1, P2, P3, P4, P5 = forward_kinematics(theta, link)
+    P1, P2, P3, P4, P5 = forward_kinematics(theta, U3)
 
     ### Plot target
     ax.scatter3D(pos[0], pos[1], pos[2])
@@ -275,7 +285,7 @@ def update_wave(val):
     plot_base(ax,L)
 
     ### Plot result
-    plot_result(ax, L, P2, P3, P4, P5)
+    plot_result(ax, L, P1, P2, P3, P4, P5)
 
     slider1.set_val(theta[0])
     slider2.set_val(theta[1])
